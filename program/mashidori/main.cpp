84a6497942c3c4e95c74211d690795e941376ce0
Tikuwa404 <mashidori2@gmail.com>
2024-05-15 17:38:30 +0900

    タイヤの進んだ距離・速度を取得
    SBDBT関連はコメントアウトしました

--- program/mashidori/main.cpp
+++ program/mashidori/main.cpp
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    main.cpp
   * @author  Tikuwa
-  * @version V0.1.0
-  * @date    12-May-2024
+  * @version V0.1.1
+  * @date    15-May-2024
   * @brief   R_B main function.
   ******************************************************************************
 */
@@ -17,16 +17,18 @@
 using std::sin;
 using std::cos;
 
-constexpr double SPD = 10.0;
+constexpr double SPD = 2.0;
+constexpr double DIAMETER = 75.0; //[mm]
+constexpr double TEST = 244;
 
 //ロボマスモータのエンコーダ受信用
 class ROBOMAS_Encoder_Data {
 public:
 	ROBOMAS_Encoder_Data() :sto({0,0,0,0}) {}
 	short operator[](int i) {
-		return sto.short_data[i];
+		return (sto.uint8_data[i*2]<<8)+sto.uint8_data[i*2+1];
 	}
-	void overwrite(uint8_t *data) {
+	void update(uint8_t *data) {
 		for (int i = 0; i < 8; ++i) {
 			sto.uint8_data[i] = data[i];
 		}
@@ -74,13 +76,13 @@ void main_interrupt(void);
 //run by can_handler
 void can_interrupt(CanRxMsgTypeDef rx) {
 	if (can_received.rx_stdid == 0x201) {
-		enc_data[0].overwrite(can_received.rx_data);
+		enc_data[0].update(can_received.rx_data);
 	}
 	if (can_received.rx_stdid == 0x202) {
-		enc_data[1].overwrite(can_received.rx_data);
+		enc_data[1].update(can_received.rx_data);
 	}
 	if (can_received.rx_stdid == 0x203) {
-		enc_data[2].overwrite(can_received.rx_data);
+		enc_data[2].update(can_received.rx_data);
 	}
 }
 
@@ -93,25 +95,32 @@ void stick_resize(double &x, double &y) {
 	y = sin(angle) * longer / 64;
 }
 
+/*
 PS3 ps3;
 PS3_data ps3_data;
+*/
 //communicate
 void get_rq(void) {
 	double rqx,rqy;
 	/*   unavailable    */
 	/*   coming soon    */
 	/* work in progress */
+	/*
 	ps3.Getdata(&ps3_data);
 	rqx = ps3_data.LxPad;
 	rqy = ps3_data.LyPad;
 	stick_resize(rqx, rqy);
+	*/
+	rqx = (sw.read()?0:1);
+	rqy = (sw.read()?1:0);
+	rq_theta = 0;
 
 	//test
 	abs_rq_p.x = rqx*SPD;
 	abs_rq_p.y = rqy*SPD;
-	rq_theta = ps3_data.RxPad / 63.0;
 }
 
+double debugd, sum = 0;
 //run every 1ms
 //モーターは正の出力で反時計回り
 void main_interrupt(void) {
@@ -123,33 +132,39 @@ void main_interrupt(void) {
 	//localize rq
 	local_rq_p = abs_rq_p;
 	local_rq_p.rotate(-direction);
-	debug1[3] = 10;
+
+	for (int i = 0; i < 4; ++i) debug1[i] = enc_data[0][i];
+	for (int i = 0; i < 4; ++i) debug2[i] = enc_data[1][i];
+	for (int i = 0; i < 4; ++i) debug3[i] = enc_data[2][i];
+	debugd = static_cast<double>(debug1[1])/36.0/60.0*TEST;
+	sum += debugd/1000.0;
 
 	//communicate with motor
 	//front
 	mtr_rq_p = local_rq_p;
 	conv.s = static_cast<short>(mtr_rq_p.x + rq_theta*SPD);
-		debug1[0] = conv.s;
 	send_data[0] = conv.u[0];
 	send_data[1] = conv.u[1];
 	//left
 	mtr_rq_p = local_rq_p;
 	mtr_rq_p.rotate(-120);
 	conv.s = static_cast<short>(mtr_rq_p.x + rq_theta*SPD);
-	debug1[1] = conv.s;
 	send_data[2] = conv.u[0];
 	send_data[3] = conv.u[1];
 	//right
 	mtr_rq_p = local_rq_p;
 	mtr_rq_p.rotate(-240);
 	conv.s = static_cast<short>(mtr_rq_p.x + rq_theta*SPD);
-	debug1[2] = conv.s;
 	send_data[4] = conv.u[0];
 	send_data[5] = conv.u[1];
 	//none
 	send_data[6] = 0;
 	send_data[7] = 0;
-	sken_system.canTransmit(CAN_2,0x200,send_data,8);
+	static int pres = 0;
+	if (++pres == 10) {
+		sken_system.canTransmit(CAN_2,0x200,send_data,8);
+		pres = 0;
+	}
 }
 
 //main
@@ -159,9 +174,9 @@ int main(void)
 	sw.init(C13,INPUT_PULLUP);
 
 	sken_system.startCanCommunicate(B13,B12,CAN_2);
-	//sken_system.addCanRceiveInterruptFunc(CAN_2,&can_received,can_interrupt);
+	sken_system.addCanRceiveInterruptFunc(CAN_2,&can_received,can_interrupt);
 
-	ps3.StartRecive();
+	//ps3.StartRecive();
 	sken_system.addTimerInterruptFunc(main_interrupt,0,1);
 
 	while (true) {}
