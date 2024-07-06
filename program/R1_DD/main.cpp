061f2423ebde84931f38e47e75855b6770baed3e
Tikuwa404 <mashidori2@gmail.com>
2024-07-06 14:21:45 +0900

    R1_DD/MDD1:細々とした修正

--- program/R1_DD/main.cpp
+++ program/R1_DD/main.cpp
@@ -2,8 +2,8 @@
   ******************************************************************************
   * @file    main.cpp
   * @author  tikuwa404
-  * @version V0.2.1
-  * @date    30-June-2024
+  * @version V0.2.2
+  * @date    06-July-2024
   * @brief   R1 DD function.
   ******************************************************************************
 */
@@ -14,28 +14,34 @@
 #include "sken_library/include.h"
 #include "myuart.hpp"
 
-MyUart<1> uartMDD1;
-//MyUart<80> uartMDD1;
+#define DD_DEBUG
+
+#ifdef DD_DEBUG
+MyUart<80> uartMDD1;
 double mtr_now[4];
 double mtr_target[4];
 int mtr_rq[4];
+#else
+MyUart<1> uartMDD1;
+#endif
 Gpio valve[3];
 PinState valve_state[3] = {LOW,LOW,LOW};
 //Uart uartMDD2;
 
 void MDD1_receive(void) {
-	/*
+#ifdef DD_DEBUG
 	union {double d[10]; int i[20]; uint8_t u8[80];} conv;
 	if (uartMDD1.read(conv.u8)) {
 		for (int i = 0; i < 4; ++i) mtr_now[i] = conv.d[i];
 		for (int i = 0; i < 4; ++i) mtr_target[i] = conv.d[i+4];
 		for (int i = 0; i < 4; ++i) mtr_rq[i] = conv.i[i+16];
 	}
-	*/
+#else
 	uint8_t tmp;
 	if (uartMDD1.read(&tmp)) {
 		for (int i = 0; i < 3; ++i) valve_state[i] = ((tmp&(0x01<<i))==(0x01<<i)) ? HIGH : LOW;
 	}
+#endif
 }
 
 void main_interrupt(void) {
