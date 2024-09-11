194d2a9142e9d9fe54e799e970f4e7bac9583f0c
Tikuwa404 <mashidori2@gmail.com>
2024-09-11 16:44:08 +0900

    CAN通信に変更

--- program/R1_sensor/main.cpp
+++ program/R1_sensor/main.cpp
@@ -12,7 +12,6 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
 #include "sken_library/include.h"
-#include "myuart.hpp"
 
 constexpr double TIRE_DIAMETER = 60; //計測輪直径[mm]
 constexpr double BODY_DIAMETER = 1027.38; //計測輪間直径[mm]
@@ -20,9 +19,19 @@ constexpr double BODY_DIAMETER = 1027.38; //計測輪間直径[mm]
 Encoder enc[3];
 Encoder_data enc_data[3];
 double posx, posy, post;
-MyUart<0> uartMDD1;
-union {float f[3]; uint8_t u8[12];} send_data;
+union {int16_t s16[3]; uint8_t u8[6];} send_data;
+Gpio led;
 
+template<typename T> void swap (T& x, T& y) {
+	T tmp;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+//上位バイト先
+//{x, y, theta}
+//stdid = 0x101
 void main_interrupt(void) {
 	for (int i=0; i<3; ++i) enc[i].interrupt(&enc_data[i]);
 	//read and update position
@@ -32,23 +41,39 @@ void main_interrupt(void) {
 	if (post > 180) post -= 360;
 	if (post <= -180) post += 360;
 
-	send_data.f[0] = (float)posx;
-	send_data.f[1] = (float)posy;
-	send_data.f[2] = (float)post;
+	send_data.s16[0] = static_cast<int16_t>(posx);
+	send_data.s16[1] = static_cast<int16_t>(posy);
+	send_data.s16[2] = static_cast<int16_t>(post);
 
-	uartMDD1.write(send_data.u8, 12);
+	/*
+	send_data.s16[0] = 314;
+	send_data.s16[1] = 1293;
+	send_data.s16[2] = 9218;
+	*/
+
+	swap(send_data.u8[0], send_data.u8[1]);
+	swap(send_data.u8[2], send_data.u8[3]);
+	swap(send_data.u8[4], send_data.u8[5]);
+
+	if (sken_system.canTransmit(CAN_2, 0x101, send_data.u8, 6)) {
+		led.write(HIGH);
+	} else {
+		led.write(LOW);
+	}
 }
 
 int main(void)
 {
 	sken_system.init();
 
+	led.init(C13, OUTPUT);
+
 	//左のコネクタからから順番に
 	enc[0].init(A0,A1,TIMER5,TIRE_DIAMETER); //左
 	enc[1].init(B3,A5,TIMER2,TIRE_DIAMETER); //後ろ
 	enc[2].init(B6,B7,TIMER4,TIRE_DIAMETER); //右
 
-	uartMDD1.init(USB_B,115200);
+	sken_system.startCanCommunicate(B13,B12,CAN_2);
 
 	sken_system.addTimerInterruptFunc(main_interrupt, 0, 1);
 
